77ebcdd5209403bcfb6f4fd02f5997aa0fa67af0
RayaneDosAnjos <raydosanjos98@gmail.com>
2023-02-27 16:15:28 -0300

    Separando arquivos(back)

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
+const posts = require('./model/posts')
 
-let posts = [
-    {id:"jhbugubu",
-    title:"Teste do mural",
-    description:"Descrição teste"
-    },
-]
+
 
 app.get("/all", (req, res)=>{
-    res.json(JSON.stringify(posts))  
+    res.json(JSON.stringify(posts.getAll()))  
 })
 
-app.post("/new", (req, res)=>{
+app.post("/new",bodyParser.json(), (req, res)=>{
+    let title = req.body.title
+    let description = req.body.description
+    
+    posts.newPost(title, description)
 
+
+    res.send("post adicionado")
 })
 
 
 app.listen(PORT, ()=>{
-    console.log(`server running on port ${PORT}`)
+    console.log("server running on port ", PORT)
 })
+
 
